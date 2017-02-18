8c33e7b611b7d95910818b17e1bd2141f59683f1
lg_paro@hotmail.com <node6564@caelum22-05>
2017-02-18 12:57:48 -0200

    Incluindo module mysql na aplicação e buscando lista de livros por este

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,17 @@
+let mysql = require('mysql');
+
 module.exports = app => {
   app.get('/produtos', (req, res) => {
-    res.render('produtos/lista');
+    let connection = mysql.createConnection({
+      host : '0.0.0.0',
+      port : 3307,
+      database : 'casadocodigo',
+      user : 'root',
+      password : ''
+    });
+
+    connection.query('select * from livros', (error, livros, fields) => {
+      res.render('produtos/lista', {livros});
+    });
   });
 };
--- config/express.js
+++ config/express.js
@@ -6,4 +6,6 @@ require('../app/routes/produtos')(app);
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(express.static('./app/public'));
+
 module.exports = app;
